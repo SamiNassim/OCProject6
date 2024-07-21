fea5cbd2c42c7524561fee68fa7cf6497c99e46c
Sami Nassim <sami.bououdine@gmail.com>
2024-07-21 19:15:10 +0200

    fix: loading property

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
-import './App.scss';
-import Banner from './components/Banner/Banner';
-import Card from './components/Card/Card';
-import React, { useEffect, useState } from 'react';
+import "./App.scss";
+import Banner from "./components/Banner/Banner";
+import Card from "./components/Card/Card";
+import React, { useEffect, useState } from "react";
 import bannerhome from "./assets/img/bannerbig.png";
 
 function App() {
-
-  const [propertyData, setPropertyData] = useState([])
+  const [propertyData, setPropertyData] = useState([]);
 
   useEffect(() => {
     fetch("./logements.json")
-      .then((response) => response.json()
-        .then((data) => setPropertyData(data))
-        .catch((error) => console.log(error))
-      )
-  }, [])
-
-  console.log(propertyData)
+      .then((response) => response.json())
+      .then((data) => {
+        setPropertyData(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
   return (
     <>
-      <Banner bannerimg={bannerhome} /><div className='main__div'>
-        {propertyData.map((property) => (
-          <Card
-            name={property.title}
-            id={property.id}
-            cover={property.cover}
-            key={property.id}
-          />
-        ))}
+      <Banner bannerimg={bannerhome} />
+      <div className="main__div">
+        {propertyData &&
+          propertyData.map((property) => (
+            <Card
+              name={property.title}
+              id={property.id}
+              cover={property.cover}
+              key={property.id}
+            />
+          ))}
       </div>
     </>
-
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,31 @@
 import logo from "./logo.png";
-import { Link, useLocation } from 'react-router-dom';
-import './Header.scss';
+import { Link, useLocation } from "react-router-dom";
+import "./Header.scss";
 
 export function Header() {
+  const location = useLocation();
 
-    const location = useLocation();
-
-    console.log(location);
-
-    return (
-        <header className="header__div">
-            <img className="header__logo" src={logo} alt="Logo Kasa" />
-            <nav>
-                <ul>
-                    <li className={`nav__list ${location.pathname === '/' && "active"}`} id="home-link" >
-                        <Link to="/">Accueil</Link>
-                    </li>
-                    <li className={`nav__list ${location.pathname === "/apropos" && "active"}`} id="about-link">
-                        <Link to="/apropos">A propos</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className="header__div">
+      <img className="header__logo" src={logo} alt="Logo Kasa" />
+      <nav>
+        <ul>
+          <li
+            className={`nav__list ${location.pathname === "/" && "active"}`}
+            id="home-link"
+          >
+            <Link to="/">Accueil</Link>
+          </li>
+          <li
+            className={`nav__list ${
+              location.pathname === "/apropos" && "active"
+            }`}
+            id="about-link"
+          >
+            <Link to="/apropos">A propos</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
